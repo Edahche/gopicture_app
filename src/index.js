db96d04831ab6107eb09f85eea4b6acf8a439b09
Edgar David Adame <90652756+Edahche@users.noreply.github.com>
2021-12-21 11:21:09 -0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const { uuid } = require('uuidv4');
+const { format } = require('timeago.js');
+
+const path = require('path');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+
+// intializations
+const app = express();
+require('./database');
+
+// settings
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 3000);
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/img/uploads'),
+    filename: (req, file, cb, filename) => {
+        console.log(file);
+        cb(null, uuid() + path.extname(file.originalname));
+    }
+})
+app.use(multer({ storage }).single('image'));
+
+// Global variables
+app.use((req, res, next) => {
+    app.locals.format = format;
+    next();
+});
+
+// routes
+app.use(require('./routes/index'));
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// start
+app.listen(3000, () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
